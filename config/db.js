e2f223b0f46b6025722ab05fc34d9ffc853e5195
Navaneeth Gade <navaneethgade07@gmail.com>
2024-09-03 00:31:36 +0530

    Database setup

---
+++ config/db.js
@@ -0,0 +1,31 @@
+const {MongoClient} = require('mongodb')
+require('dotenv').config();
+
+const uri = process.env.MONGO_URI;
+const dbName = 'DTNodejs_project';
+const collectionName = 'mydocuments';
+
+const client = new MongoClient(uri);
+
+async function main() {
+    try{
+        await client.connect();
+        console.log('Database connected Successfully');
+
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+
+        return{db , collection};
+    }
+    catch(err){
+        console.log(err);      
+        return null;
+    }
+    // finally{
+    //     await client.close();
+    //     console.log('Database connection closed');
+    // }
+    
+};
+
+module.exports = {main};
